96b4c64d71670928787f983cf7e7ab0348230a71
BerkatPS <xolderx@outlook.co.id>
2022-05-11 23:23:48 +0700

    Mengubah Tampilan Mobile Pada Navbar

--- Teacher/index.php
+++ Teacher/index.php
@@ -146,8 +146,8 @@ if($time['tm_hour'] > 8){
                             @click= "isOpen = !isOpen"
                             class="flex items-center pb-2 focus:outline-none ">
                                 <div class="gap-3 relative flex md:text-base">
-                                    <span class="flex space-y-2 text-sm text-slate-400"><?= $_SESSION['Teacher']; ?></span> 
-                                   <span class="absolute pt-5 pl-1 text-xs items-center text-slate-400">TEACHER</span>
+                                    <span class="flex space-y-2 text-sm md:text-lg text-slate-400"><?= $_SESSION['Teacher']; ?></span> 
+                                   <span class="absolute pt-5 pl-1 text-xs md:text-base items-center text-slate-400">TEACHER</span>
                                    <img src="https://raw.githubusercontent.com/sefyudem/Responsive-Login-Form/master/img/avatar.svg" class="rounded-full flex h-10 w-10 gap-2 pl-2" alt="image" srcset="">
                                 </div>
                                 <div class="relative">
