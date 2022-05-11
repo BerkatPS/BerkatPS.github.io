a6167bcab6c2472af028127fa52abc88d951cd52
BerkatPS <xolderx@outlook.co.id>
2022-05-11 22:38:21 +0700

    Mengubah Ukuran Font Sidebar

--- Teacher/components/arsip_mengajar.php
+++ Teacher/components/arsip_mengajar.php
@@ -39,7 +39,7 @@ $tbl_daftar_pelajaran = $confg->query("SELECT tbl_pelajaran.id AS id_pelajaran_s
              </div>
             </a>
             <div class="relative">
-                <nav class="text-slate-400 min-h-screen overflow-y-auto font-mono text-base relative pt-7 gap-3 md:text-lg">
+                <nav class="text-slate-400 min-h-screen overflow-y-auto font-mono text-base relative pt-7 gap-3 text-sm md:text-lg">
                 <a href="../" class="flex items-center gap-2 text-zinc-300 py-2 px-3 my-5 hover:bg-indigo-500 rounded-md transition duration-200">
                     <div class="flex items-center">
                         <img src="../../icons/layout.png" class="h-6 w-6"alt="">
--- Teacher/components/daftar_absensi_form.php
+++ Teacher/components/daftar_absensi_form.php
@@ -64,7 +64,7 @@ if(isset($_POST['submitabsen'])){
              </div>
             </a>
             <div class="relative">
-                <nav class="text-slate-400 min-h-screen overflow-y-auto font-mono text-base relative pt-7 gap-3 md:text-lg">
+                <nav class="text-slate-400 min-h-screen overflow-y-auto font-mono text-base relative pt-7 gap-3 text-sm md:text-lg">
                 <a href="../" class="flex items-center gap-2 text-zinc-300 py-2 px-3 my-5 hover:bg-indigo-500 rounded-md transition duration-200">
                     <div class="flex items-center">
                         <img src="../../icons/layout.png" class="h-6 w-6"alt="">
